5af27ed7c211dcd42a22d9dd246b3b3f45987c46
Julien Durillon <julien.durillon@gmail.com>
2014-01-27 18:56:59 +0100

    Add doc

--- app/models/entities.scala
+++ app/models/entities.scala
@@ -1,41 +1,66 @@
 package models
 
+/**
+ * These are the classes that are needed to run a simple addon API.
+ * As we want to integrate seamlessly with the heroku addon API to
+ * permit vendors to get faster to work, expect to receive jsons with
+ * Heroku-flagged fields.
+ **/
+
+/**
+ * This is sent by the user (that is, Clever Cloud's server) to
+ * provision an addon.
+ **/
 case class ProvisionData(
-	heroku_id: String,
-  	plan: String,
-  	region: String,
-	callback_url: String,
-	logplex_token: String,
+	heroku_id: String, // Id of the application/addon from Clever Cloud's point of view
+  	plan: String, // Wanted plan's name.
+  	region: String, // Region. For now, only EU is valid.
+	callback_url: String, // URL to request user's data to the Clever Cloud's API
+	logplex_token: String, // Token you need to send logs to our logging system.
 	options: Map[String,String] // These are only here to add parameters to creation. No need to store'em
 )
 
+/**
+ * This is used for respond to provision requests.
+ * If we respond an error, the id field will be "error"
+ **/
 case class ProvisionResponse(
-	id: String,
-	message: String,
-	config: Map[String,String] = Map.empty
+	id: String, // The addon id from this API POV.
+	message: String, // A creation message
+	config: Map[String,String] = Map.empty // config data (variables to be injected into applications to contact the service you provide
 )
 
+/**
+ * This is sent by Clever's API to update an addon's plan.
+ */
 case class PlanChangeData(
-	heroku_id: String,
-	plan: String
+	heroku_id: String, // The addon's id from Clever's POV.
+	plan: String // new plan name.
 )
 
+/**
+ * This is an internal representation of an addon.
+ * It's kind of a mix between ProvisionData and ProvisionRespons
+ */
 case class AddonData(
-	id: String,
-	appId: String,
-  	plan: String,
-  	region: String,
-	callback_url: String,
-	logplex_token: String,
-	config: Map[String,String]
+	id: String, // Id from the provider's POV.
+	appId: String, // Id from the Clever's POV.
+  	plan: String, // Plan name.
+  	region: String, // Region (so, for now, EU).
+	callback_url: String, // URL to request user's data to the Clever Cloud's API
+	logplex_token: String, // Token you need to send logs to our logging system.
+	config: Map[String,String] // These are only here to add parameters to creation. No need to store'em
 )
 
+/**
+ * Class to map an SSO request's params.
+ */
 case class SSOData(
-	id: String,
-	timestamp: Long,
-	token: String,
-	navData: String,
-	email: String
+	id: String, // Id from the provider's POV.
+	timestamp: Long, // timestamp of the token creation. In millis.
+	token: String, // token used to sign the request.
+	navData: String, // Some data to put in the session.
+	email: String // email of the requesting user.
 )
 
 
@@ -45,7 +70,6 @@ object JsonFormats {
 	import play.api.data.validation.ValidationError
 
 	implicit val provisionDataFormat = Json.format[ProvisionData]
-	implicit val addonDataFormat = Json.format[AddonData]
 	implicit val provisionResponseFormat = Json.format[ProvisionResponse]
 	implicit val planChangeFormat = Json.format[PlanChangeData]
 }
