a1db1ab15fbcc337a0deb2a9e48740466d776943
Julien Durillon <julien.durillon@gmail.com>
2014-02-05 12:22:30 +0100

    return random values

--- app/models/MyProvisionDriver.scala
+++ app/models/MyProvisionDriver.scala
@@ -4,17 +4,18 @@ import utils.ProvisionDriverModule
 
 trait MyProvisionDriver extends ProvisionDriverModule {
 	def driver = new ProvisionDriver {
+		import org.apache.commons.lang3.RandomStringUtils
 		def provision(pd: ProvisionData): Either[String,AddonData] = Right(
 			AddonData(
-				"myaddon_dummy",
+				"myaddon_"+java.util.UUID.randomUUID().toString,
 				pd.heroku_id,
 				pd.plan,
 				pd.region,
 				pd.callback_url,
 				pd.logplex_token,
 				Map(
-					"MY_ADDON_USER" -> "someuser",
-				  	"MY_ADDON_PASSWORD" -> "somepassword"
+					"MY_ADDON_USER" -> RandomStringUtils.randomAlphanumeric(10),
+				  	"MY_ADDON_PASSWORD" -> RandomStringUtils.randomAlphanumeric(20)
 				)
 			)
 		)
--- build.sbt
+++ build.sbt
@@ -8,7 +8,8 @@ libraryDependencies ++= Seq(
   cache,
   "joda-time" % "joda-time" % "2.3",
   "org.postgresql" % "postgresql" % "9.3-1100-jdbc41",
-  "commons-codec" % "commons-codec" % "1.9"
+  "commons-codec" % "commons-codec" % "1.9",
+  "org.apache.commons" % "commons-lang3" % "3.2.1"
 )
 
 scalacOptions ++= Seq("-feature")
